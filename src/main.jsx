b01b6408759b7e6a3723c22c1e7caa312e0403d5
Asad <asad2747982@gmail.com>
2026-04-24 05:09:01 +0500

    ci cd issue fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/token-management-system">
+    <BrowserRouter basename="/Token-Generator-Project/">
       <App />
     </BrowserRouter>
   </StrictMode>
